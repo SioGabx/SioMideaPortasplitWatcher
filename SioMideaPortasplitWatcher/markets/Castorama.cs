ec8451e28798270960a520fd09cbd49e0eb2faff
SioGabx <41244237+SioGabx@users.noreply.github.com>
2026-06-25 10:21:23 +0200

    need to add castorama

---
+++ SioMideaPortasplitWatcher/markets/Castorama.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SioMideaPortasplitWatcher.markets
+{
+    internal class Castorama
+    {
+//        Invoke-WebRequest -UseBasicParsing -Uri "https://api.kingfisher.com/v1/mobile/stores/CAFR?nearLatLong=48.655805%2C6.181587&page%5Bsize%5D=10&include=clickAndCollect%2Cstock&filter%5Bean%5D=8431312260509" `
+//-UserAgent "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:152.0) Gecko/20100101 Firefox/152.0" `
+//-Headers @{
+//"Accept" = "*/*"
+//  "Accept-Language" = "fr,fr-FR;q=0.9,en-US;q=0.8,en;q=0.7"
+//  "Accept-Encoding" = "gzip, deflate, br, zstd"
+//  "Referer" = "https://www.castorama.fr/"
+//  "Authorization" = "Atmosphere atmosphere_app_id=kingfisher-o4ITR0sWAyCVQBraQf4Es61jHV3dN4oO9UwJQMrS"
+//  "traceparent" = "00-0000000000000000c4dc3589d19544b9-2d3d55c7fe8af424-01, 00-000000000000000024a18935517751dc-745831de66110b92-01"
+//  "tracestate" = "dd=s:1;o:rum, dd=s:1;o:rum"
+//  "x-datadog-origin" = "rum, rum"
+//  "x-datadog-parent-id" = "3259856022743479332, 8383505537615072146"
+//  "x-datadog-sampling-priority" = "1, 1"
+//  "x-datadog-trace-id" = "14185271792353232057, 2639541718708867548"
+//  "Priority" = "u=4"
+//  "Pragma" = "no-cache"
+//  "Cache-Control" = "no-cache"
+//  "Origin" = "https://www.castorama.fr"
+//  "Sec-Fetch-Storage-Access" = "none"
+//  "Sec-Fetch-Dest" = "empty"
+//  "Sec-Fetch-Mode" = "cors"
+//  "Sec-Fetch-Site" = "cross-site"
+//}
+    }
+}
